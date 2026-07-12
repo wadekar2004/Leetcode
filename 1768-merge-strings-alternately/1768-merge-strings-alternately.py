6a551b1199766c8f9aeaf53dbfb5f88ddc976750
wadekar2004 <a.wadekar2004@gmail.com>
2026-07-12 18:28:12 +0530

    Time: 51 ms (19.91%), Space: 19.5 MB (17%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -4,10 +4,11 @@ class Solution:
 
         for i in range(min(len(word1),len(word2))):
             result+=word1[i]
-            result += word2[i]
+            result+=word2[i]
 
-        result += word1[len(word2):]
-        result += word2[len(word1):]
 
+        result+=word1[len(word2):]
+        result+=word2[len(word1):]
+ 
         return result
-        
+
