93c8838c054a2304ca06fa2fa84a725d3640a966
wadekar2004 <a.wadekar2004@gmail.com>
2026-07-31 19:59:18 +0530

    Time: 82 ms (73.23%), Space: 62.4 MB (27.8%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
@@ -17,8 +17,64 @@ class Solution:
             slow=slow.next
             fast=fast.next.next
         prev.next=slow.next
-
         return head
+# Use 2 pointers.
+# One is
+# Slow
+# Another
+# Fast
+# Rule
+# Slow
+# moves 1 step
+# ↓
 
+# Fast
 
+# moves 2 steps
+
+# Suppose
+
+# 1 → 3 → 4 → 7 → 1 → 2 → 6
+
+# Initially
+
+# S
+# F
+
+# ↓
+
+# 1 → 3 → 4 → 7 → 1 → 2 → 6
+# Move 1
+
+# Slow
+
+# 3
+
+# Fast
+
+# 4
+# 1 → 3 → 4 → 7 → 1 → 2 → 6
+#     S   F
+# Move 2
+
+# Slow
+
+# 4
+
+# Fast
+
+# 1
+# 1 → 3 → 4 → 7 → 1 → 2 → 6
+#         S       F
+# Move 3
+
+# Slow
+
+# 7
+
+# Fast
+
+# 6
+# 1 → 3 → 4 → 7 → 1 → 2 → 6
+#             S           F
         
