8695c46c7d55d599273337b6a4447cbeb5b77915
wadekar2004 <a.wadekar2004@gmail.com>
2026-08-08 14:18:48 +0530

    Time: 444 ms (15.95%), Space: 21 MB (8.49%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        
+        def dfs(node,total):
+            if node is None:
+                return 0
+            total+=node.val
+
+            count=0
+
+            if total==targetSum:
+                count=1
+            count+=dfs(node.left,total)
+            count+=dfs(node.right,total)
+
+            return count
+        if root is None:
+            return 0
+
+            #start next node change node
+
+        return (
+            dfs(root,0)
+            +self.pathSum(root.left,targetSum)
+            +self.pathSum(root.right,targetSum)
+        )
